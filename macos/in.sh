25d26b6119830fc8253877231117443f04ca6908
Thiago Perrotta <tbperrotta@gmail.com>
2022-09-25 16:58:55 +0200

    macos: introduce in.sh with brew bundle dump

---
+++ macos/in.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# generate Brewfile
+brew bundle dump
--- macos/out.sh
+++ macos/out.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 # https://stackoverflow.com/questions/98310/focus-follows-mouse-plus-auto-raise-on-mac-os-x
 defaults write com.apple.Terminal FocusFollowsMouse -bool true
